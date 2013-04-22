47d13ccbba46ba13ae315c4f4028d7f9e8bfba8d
Joseph Bass <jab416171@gmail.com>
2013-04-22 10:52:18 -0600

    Update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,5 @@
 mv * .[^.]* ~
-sudo apt-get -y install git vim ack acpi
+sudo apt-get -y install git vim ack-grep acpi
 mkdir -p ~/.vim/bundle/vundle
 git clone git@github.com:gmarik/vundle.git ~/.vim/bundle/vundle
 rm -rf dotfiles/
